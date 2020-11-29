d517e581151f71ece485c461d012414c7fa90d0e
Dmitriy Kovalenko <dzimitron@gmail.com>
2020-11-29 22:49:16 +0300

    feat: add interactive header navigation buttons

--- markup/js/script.js
+++ markup/js/script.js
@@ -2,11 +2,11 @@ document.addEventListener('scroll', onscroll);
 
 function onscroll(event) {
   const curPos = window.scrollY;
-  const sections = document.querySelectorAll('section');
   const links = document.querySelectorAll('.navigation a');
+  const anchor = document.querySelectorAll('.anchor');
 
-  sections.forEach((el) => {
-    if (el.offsetTop <= curPos && (el.offsetTop + el.offsetHeight) > curPos) {
+  anchor.forEach((el) => {
+    if (el.parentElement.offsetTop - 95 <= curPos && (el.parentElement.offsetTop -95 + (el.parentElement.offsetHeight)) > curPos) {
       links.forEach((a) => {
         a.classList.remove('active');
         if (el.getAttribute('id') === a.getAttribute('href').substring(1)) {
