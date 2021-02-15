f8c46d23e42b9dbb2291eac06d4228ff76232954
Dmitriy Kovalenko <dzimitron@gmail.com>
2021-02-15 11:15:10 +0300

    refactor: optimize js code and change logic button demo

--- drum-kit/main.js
+++ drum-kit/main.js
@@ -1,13 +1,24 @@
 const app = () => {
 
+  const auto = document.querySelector('.auto'),
+    keys = document.querySelector('.keys'),
+    TIMEOUT_DURATION = 250,
+    PLAY_KICK_IN_BITS = 3,
+    PLAY_SNARE_IN_BITS = 1,
+    BITS_IN_TIME = 8,
+    INTERVAL_BETWEEN_SOUNDS = 4,
+    KICK = 68,
+    SNARE = 74,
+    HIHAT = 83;
+  let tact = 1;  
+
   function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    const audio = document.querySelector(`audio[data-key="${e}"]`);
+    const key = document.querySelector(`.key[data-key="${e}"]`);
     if (!audio) return;
-    audio.currentTime = 0;  
+    audio.currentTime = 0;
     audio.play();
     key.classList.add('playing');
-    console.log(e);
   }
 
   function removeTransition(e) {
@@ -15,45 +26,32 @@ const app = () => {
     this.classList.remove('playing');
   }
   
-  const keys = document.querySelectorAll('.key');
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-  window.addEventListener('keydown', playSound);
+  keys.childNodes.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', e => playSound(e.keyCode));
   
-  keys.forEach(el => el.addEventListener('click', (e) => {
-    let atr = e.target.getAttribute('data-key');
-    playSelectKey(atr);
-  }));
-
-  function playSelectKey(k) {
-    const audio = document.querySelector(`audio[data-key="${k}"]`);
-    const key = document.querySelector(`.key[data-key="${k}"]`);
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add('playing');
-  }
-
-  const auto = document.querySelector('.auto');
-  auto.addEventListener('click', () => {
-
-    let n = 0;
-    function autoPlay() {
-      n < 8 ? n++ : n = 1;
-      if (n % 3 === 0 || n === 7) {
-        playSelectKey(68);
-      } if (n === 5 || n === 1) {
-        playSelectKey(74);
-      }
-      playSelectKey(83);
-      if (auto.classList.contains('active')){
-        setTimeout(autoPlay, 250)
-      }
-    } 
-    auto.classList.toggle('active');
-    if (auto.classList.contains('active')) {
-      autoPlay();
-    }
-
+  keys.addEventListener('click', function({target}) {
+    const key = target.closest('[data-key]');
+    if (!key) return;
+    const atr = key.getAttribute('data-key');
+    playSound(atr);
   });
 
-};
+  function autoPlayDrum() {
+    tact < BITS_IN_TIME ? tact++ : tact = 1;
+    if (tact % PLAY_KICK_IN_BITS === 0 || tact === PLAY_KICK_IN_BITS + INTERVAL_BETWEEN_SOUNDS) {
+      playSound(KICK);
+    } if (tact === PLAY_SNARE_IN_BITS || tact === PLAY_SNARE_IN_BITS + INTERVAL_BETWEEN_SOUNDS) {
+      playSound(SNARE);
+    }
+    playSound(HIHAT);
+    if (auto.classList.contains('active')) setTimeout(autoPlayDrum, TIMEOUT_DURATION);
+  }
+
+  auto.addEventListener('click', () => {
+    auto.classList.toggle('active');
+    if (auto.classList.contains('active')) autoPlayDrum();
+  });
+
+}
+
 app();
